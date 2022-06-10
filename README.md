580df91e0eb8198be4af10468e80ec17ec888967
Nathan Foreman <nforeman@ryanrts.com>
2022-06-10 09:14:19 -0500

    Succseful test

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
  - https://aws.amazon.com/about-aws/whats-new/2021/12/aws-sdk-rust-developer-preview/
  - https://github.com/awslabs/aws-sdk-rust
     - https://www.reddit.com/r/rust/comments/a1jkof/rust_support_has_been_added_to_aws_lambda/
+
+
+   
--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ use serde_json::{json, Value};
 // Taken From - https://github.com/awslabs/aws-lambda-rust-runtime
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let func = service_fn(func);
-    lambda_runtime::run(func).await?;
+    let handler = service_fn(handler);
+    lambda_runtime::run(handler).await?;
     Ok(())
 }
 
-async fn func(event: LambdaEvent<Value>) -> Result<Value, Error> {
+async fn handler(event: LambdaEvent<Value>) -> Result<Value, Error> {
     let (event, _context) = event.into_parts();
     let first_name = event["firstName"].as_str().unwrap_or("world");
-
+    println!("Respoding");
     Ok(json!({ "message": format!("Hello, {}!", first_name) }))
 }
